945986a8ea64c4bff05bb33e853f13c2725442e8
GimpG1 <karool.dwojak@gmail.com>
2022-12-02 10:12:29 +0100

    change usings and namespaces to the .net 6 format

--- CaloriesCalculator.Tests/ActivityLevelTests.cs
+++ CaloriesCalculator.Tests/ActivityLevelTests.cs
@@ -1,95 +1,86 @@
-using CaloriesCalculator.Structure;
+namespace CaloriesCalculator.Tests;
 
-namespace CaloriesCalculator.Tests
+public class ActivityLevelTests
 {
-    public class ActivityLevelTests
+    [Theory]
+    [InlineData(Protos.SexOrientation.Female)]
+    [InlineData(Protos.SexOrientation.Male)]
+    public void When_NoneActivitySet_Then_ExpectSameResult(Protos.SexOrientation orientation)
     {
-        [Theory]
-        [InlineData(Protos.SexOrientation.Female)]
-        [InlineData(Protos.SexOrientation.Male)]
-        public void When_NoneActivitySet_Then_ExpectSameResult(Protos.SexOrientation orientation)
-        {
-            ActivityLevel al = new();
+        ActivityLevel al = new();
+        Assert.Equal(ActivityValues.None, al.GetActivityFor(Protos.ActivityLevel.None, orientation));
+    }
 
-            Assert.Equal(ActivityValues.None, al.GetActivityFor(Protos.ActivityLevel.None, orientation));
+    [Theory]
+    [InlineData(Protos.SexOrientation.Female)]
+    [InlineData(Protos.SexOrientation.Male)]
+    public void When_VeryLowActivitySet_Then_ExpectSameResult(Protos.SexOrientation orientation)
+    {
+        ActivityLevel al = new();
+        Assert.Equal(ActivityValues.VeryLow, al.GetActivityFor(Protos.ActivityLevel.Verylow, orientation));
+    }
+
+    [Theory]
+    [InlineData(Protos.SexOrientation.Female)]
+    [InlineData(Protos.SexOrientation.Male)]
+    public void When_LowActivitySet_Then_CheckTheResults(Protos.SexOrientation orientation)
+    {
+        ActivityLevel al = new();
+        if (orientation == Protos.SexOrientation.Female)
+        {
+            Assert.Equal(ActivityValues.Female.Low, al.GetActivityFor(Protos.ActivityLevel.Low, orientation));
         }
-
-        [Theory]
-        [InlineData(Protos.SexOrientation.Female)]
-        [InlineData(Protos.SexOrientation.Male)]
-        public void When_VeryLowActivitySet_Then_ExpectSameResult(Protos.SexOrientation orientation)
+        else
         {
-            ActivityLevel al = new();
-
-            Assert.Equal(ActivityValues.VeryLow, al.GetActivityFor(Protos.ActivityLevel.Verylow, orientation));
+            Assert.Equal(ActivityValues.Male.Low, al.GetActivityFor(Protos.ActivityLevel.Low, orientation));
         }
+    }
 
-        [Theory]
-        [InlineData(Protos.SexOrientation.Female)]
-        [InlineData(Protos.SexOrientation.Male)]
-        public void When_LowActivitySet_Then_CheckTheResults(Protos.SexOrientation orientation)
+    [Theory]
+    [InlineData(Protos.SexOrientation.Female)]
+    [InlineData(Protos.SexOrientation.Male)]
+    public void When_AverageActivitySet_Then_CheckTheResults(Protos.SexOrientation orientation)
+    {
+        ActivityLevel al = new();
+        if (orientation == Protos.SexOrientation.Female)
         {
-            ActivityLevel al = new();
-
-            if (orientation == Protos.SexOrientation.Female)
-            {
-                Assert.Equal(ActivityValues.Female.Low, al.GetActivityFor(Protos.ActivityLevel.Low, orientation));
-            }
-            else
-            {
-                Assert.Equal(ActivityValues.Male.Low, al.GetActivityFor(Protos.ActivityLevel.Low, orientation));
-            }
+            Assert.Equal(ActivityValues.Female.Average, al.GetActivityFor(Protos.ActivityLevel.Average, orientation));
         }
-
-        [Theory]
-        [InlineData(Protos.SexOrientation.Female)]
-        [InlineData(Protos.SexOrientation.Male)]
-        public void When_AverageActivitySet_Then_CheckTheResults(Protos.SexOrientation orientation)
+        else
         {
-            ActivityLevel al = new();
-
-            if (orientation == Protos.SexOrientation.Female)
-            {
-                Assert.Equal(ActivityValues.Female.Average, al.GetActivityFor(Protos.ActivityLevel.Average, orientation));
-            }
-            else
-            {
-                Assert.Equal(ActivityValues.Male.Average, al.GetActivityFor(Protos.ActivityLevel.Average, orientation));
-            }
+            Assert.Equal(ActivityValues.Male.Average, al.GetActivityFor(Protos.ActivityLevel.Average, orientation));
         }
+    }
 
-        [Theory]
-        [InlineData(Protos.SexOrientation.Female)]
-        [InlineData(Protos.SexOrientation.Male)]
-        public void When_HighActivitySet_Then_CheckTheResults(Protos.SexOrientation orientation)
+    [Theory]
+    [InlineData(Protos.SexOrientation.Female)]
+    [InlineData(Protos.SexOrientation.Male)]
+    public void When_HighActivitySet_Then_CheckTheResults(Protos.SexOrientation orientation)
+    {
+        ActivityLevel al = new();
+        if (orientation == Protos.SexOrientation.Female)
         {
-            ActivityLevel al = new();
-
-            if (orientation == Protos.SexOrientation.Female)
-            {
-                Assert.Equal(ActivityValues.Female.High, al.GetActivityFor(Protos.ActivityLevel.High, orientation));
-            }
-            else
-            {
-                Assert.Equal(ActivityValues.Male.High, al.GetActivityFor(Protos.ActivityLevel.High, orientation));
-            }
+            Assert.Equal(ActivityValues.Female.High, al.GetActivityFor(Protos.ActivityLevel.High, orientation));
         }
-
-        [Theory]
-        [InlineData(Protos.SexOrientation.Female)]
-        [InlineData(Protos.SexOrientation.Male)]
-        public void When_VeryHighActivitySet_Then_CheckTheResults(Protos.SexOrientation orientation)
+        else
         {
-            ActivityLevel al = new();
+            Assert.Equal(ActivityValues.Male.High, al.GetActivityFor(Protos.ActivityLevel.High, orientation));
+        }
+    }
 
-            if (orientation == Protos.SexOrientation.Female)
-            {
-                Assert.Equal(ActivityValues.Female.Veryhigh, al.GetActivityFor(Protos.ActivityLevel.Veryhigh, orientation));
-            }
-            else
-            {
-                Assert.Equal(ActivityValues.Male.Veryhigh, al.GetActivityFor(Protos.ActivityLevel.Veryhigh, orientation));
-            }
+    [Theory]
+    [InlineData(Protos.SexOrientation.Female)]
+    [InlineData(Protos.SexOrientation.Male)]
+    public void When_VeryHighActivitySet_Then_CheckTheResults(Protos.SexOrientation orientation)
+    {
+        ActivityLevel al = new();
+        if (orientation == Protos.SexOrientation.Female)
+        {
+            Assert.Equal(ActivityValues.Female.Veryhigh, al.GetActivityFor(Protos.ActivityLevel.Veryhigh, orientation));
+        }
+        else
+        {
+            Assert.Equal(ActivityValues.Male.Veryhigh, al.GetActivityFor(Protos.ActivityLevel.Veryhigh, orientation));
         }
     }
 }
--- CaloriesCalculator.Tests/CalculateTests.cs
+++ CaloriesCalculator.Tests/CalculateTests.cs
@@ -1,42 +1,34 @@
 using CaloriesCalculator.Protos;
-using CaloriesCalculator.Structure;
 using Moq;
 
-namespace CaloriesCalculator.Tests
+namespace CaloriesCalculator.Tests;
+public class CalculateTests
 {
-    public class CalculateTests
-    {
-        [Theory]
-        [InlineData(SexOrientation.Male, 1_809.67f ,80f, 170f, 30f)]
-        [InlineData(SexOrientation.Female, 1_383.46f ,55f, 170f, 24f)]
-        public void Check_IfCanCalculate_PPM(SexOrientation orientation, float ppm, float weight, float height, uint age)
-        {        
-            var helper = new HelperCalculationsTests();
-            var _mActivity = new Mock<IActivity>();
+    [Theory]
+    [InlineData(SexOrientation.Male, 1_809.67f ,80f, 170f, 30f)]
+    [InlineData(SexOrientation.Female, 1_383.46f ,55f, 170f, 24f)]
+    public void Check_IfCanCalculate_BaseMetabolism(SexOrientation orientation, float ppm, float weight, float height, uint age)
+    {        
+        var helper = new HelperCalculationsTests();
+        var _mActivity = new Mock<IActivity>();
+        var calculationData = helper.CreateBaseCalculationFor(orientation, _mActivity.Object);  
+        Assert.NotNull(calculationData);
+        Assert.Equal(ppm, calculationData.CalculateBasicMetabolism(weight, height, age));
+    }
 
-            var calculationData = helper.CreateBaseCalculationFor(orientation, _mActivity.Object);  
-
-            Assert.NotNull(calculationData);
-            Assert.Equal(ppm, calculationData.CalculateBasicMetabolism(weight, height, age));
-        }
-
-        [Theory]
-        [InlineData(SexOrientation.Male, Protos.ActivityLevel.Verylow, 2_171.60f, 1_809.67f ,80f, 170f, 30f)]
-        [InlineData(SexOrientation.Female, Protos.ActivityLevel.Verylow, 1_660.16f, 1_383.46f ,55f, 170f, 24f)]
-        public void Check_IfCanCalculate_CPM(SexOrientation orientation, Protos.ActivityLevel activity, float cpm, float ppm, float weight, float height, uint age)
-        {          
-            var helper = new HelperCalculationsTests();
-            var _mActivity = new Mock<IActivity>();
-            _mActivity.Setup(item=>item.GetActivityFor(It.IsAny<Protos.ActivityLevel>(), It.IsAny<SexOrientation>()))
-                      .Returns(helper.GetActivityValueFor(orientation, activity));
-
-            var calculationData = helper.CreateBaseCalculationFor(orientation, _mActivity.Object);  
-
-            Assert.NotNull(calculationData);
-            Assert.Equal(ppm, calculationData.CalculateBasicMetabolism(weight, height, age));
-
-            Assert.True(calculationData.TryCalculateTotalMetabolism(activity, orientation, out float cpmResult));
-            Assert.Equal(cpm, cpmResult);
-        }
+    [Theory]
+    [InlineData(SexOrientation.Male, Protos.ActivityLevel.Verylow, 2_171.60f, 1_809.67f ,80f, 170f, 30f)]
+    [InlineData(SexOrientation.Female, Protos.ActivityLevel.Verylow, 1_660.16f, 1_383.46f ,55f, 170f, 24f)]
+    public void Check_IfCanCalculate_TotalMetabolism(SexOrientation orientation, Protos.ActivityLevel activity, float cpm, float ppm, float weight, float height, uint age)
+    {          
+        var helper = new HelperCalculationsTests();
+        var _mActivity = new Mock<IActivity>();
+        _mActivity.Setup(item=>item.GetActivityFor(It.IsAny<Protos.ActivityLevel>(), It.IsAny<SexOrientation>()))
+                  .Returns(helper.GetActivityValueFor(orientation, activity));
+        var calculationData = helper.CreateBaseCalculationFor(orientation, _mActivity.Object);  
+        Assert.NotNull(calculationData);
+        Assert.Equal(ppm, calculationData.CalculateBasicMetabolism(weight, height, age));
+        Assert.True(calculationData.TryCalculateTotalMetabolism(activity, orientation, out float cpmResult));
+        Assert.Equal(cpm, cpmResult);
     }
 }
